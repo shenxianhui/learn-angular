595f8e415c2a7eeb19c5a7e4f3a2e8a9fee20860
shenxh <xianhui.shen@eslink.com>
2024-05-20 10:31:39 +0800

    feat: 组件修改

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,17 +1,33 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.less'],
+  selector: "app-child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.less"],
 })
-export class ChildComponent {
-  @Input() title!: string
-  @Output() clickTitle = new EventEmitter()
+export class ChildComponent implements OnInit {
+  @Input() title!: string;
+  @Output() clickTitle = new EventEmitter();
 
-  content = '内容'
+  content = "内容";
+  list = [
+    {
+      name: "张三",
+    },
+    {
+      name: "李四",
+    },
+    {
+      name: "王五",
+    },
+  ];
+  show = true;
+
+  constructor() {}
+
+  ngOnInit() {}
 
   onClickContent() {
-    alert('点击内容')
+    alert("点击内容");
   }
 }
