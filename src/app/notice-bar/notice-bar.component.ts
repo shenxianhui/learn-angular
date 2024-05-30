43c227a6d9c885fd44986050c4def7b6d8883e6f
shenxh <shenxh0928@qq.com>
2024-05-30 22:54:36 +0800

    feat: 修改notice-bar

--- src/app/notice-bar/notice-bar.component.ts
+++ src/app/notice-bar/notice-bar.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, Input, ElementRef, ViewChild, AfterViewInit } from '@angular/core'
+import {
+  Component,
+  OnInit,
+  Input,
+  ElementRef,
+  ViewChildren,
+  AfterViewInit,
+  QueryList,
+  HostListener,
+} from '@angular/core'
 
 @Component({
   selector: 'app-notice-bar',
@@ -10,21 +19,40 @@ export class NoticeBarComponent implements OnInit, AfterViewInit {
   @Input() speed: number = 20 // 滚动速率 (px/s)
   @Input() scrollable: boolean = true // 开启自动滚动
 
-  @ViewChild('noticeBarContent') contentElement!: ElementRef
+  @ViewChildren('noticeBarContent') contentElements!: QueryList<ElementRef>
+
+  noticeList = ['内容1内容1内容1内容1内容1内容1', '内容2内容2内容2', '内容3']
 
   constructor() {}
 
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    this.startScrolling()
+    this.checkContentOverflow()
   }
 
-  startScrolling(): void {
-    if (this.scrollable) {
-      const contentWidth = this.contentElement.nativeElement.offsetWidth
-      const animationDuration = contentWidth / this.speed
-      this.contentElement.nativeElement.style.animationDuration = `${animationDuration}s`
-    }
+  checkContentOverflow(): void {
+    this.contentElements.forEach(contentElement => {
+      const containerWidth = contentElement.nativeElement.parentElement.offsetWidth
+      const contentWidth = contentElement.nativeElement.offsetWidth
+      if (contentWidth > containerWidth) {
+        contentElement.nativeElement.parentElement.classList.add('scrollable')
+        this.updateAnimationDuration(contentElement)
+      } else {
+        contentElement.nativeElement.parentElement.classList.remove('scrollable')
+        contentElement.nativeElement.style.transform = 'translateX(0%)'
+      }
+    })
+  }
+
+  updateAnimationDuration(contentElement: ElementRef): void {
+    const contentWidth = contentElement.nativeElement.offsetWidth
+    const animationDuration = contentWidth / this.speed
+    contentElement.nativeElement.style.animationDuration = `${animationDuration}s`
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.checkContentOverflow()
   }
 }
