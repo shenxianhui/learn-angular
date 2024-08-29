ea30dd2572bca075c997c41e40005372ee6a049f
shenxh <xianhui.shen@eslink.com>
2024-08-29 15:06:00 +0800

    feat: line-bar 联调

--- src/app/line-bar/line-bar.component.ts
+++ src/app/line-bar/line-bar.component.ts
@@ -1,5 +1,88 @@
 import { Component, OnInit } from '@angular/core'
 import * as echarts from 'echarts'
+import { json_data } from './data.json'
+import { fn } from '@angular/compiler/src/output/output_ast'
+
+const { data_json = {}, color_json = {} } = json_data
+const { data = {} } = data_json
+const { legend = {}, seriesMap = {} } = color_json
+const { xAxis = {}, series = [] } = data
+const { data: xAxisData = [] } = xAxis
+const seriesData = []
+
+function getBorderHeight(list = []) {
+  const sum = list.reduce((acc, curr) => acc + curr, 0)
+  const average = sum / list.length / 40
+  const arr = Array(list.length).fill(average)
+
+  return arr
+}
+
+series.forEach(item => {
+  if (item.type === 'line') {
+    seriesData.push({
+      ...item,
+      step: true,
+    })
+  }
+  if (item.type === 'bar') {
+    const styleObj = seriesMap[item.key]
+
+    if (styleObj.borderColor) {
+      seriesData.push(
+        {
+          ...item,
+          stack: 1,
+          barGap: '0',
+          borderWidth: 0,
+          itemStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(
+                0,
+                0,
+                0,
+                1,
+                [
+                  {
+                    offset: 0,
+                    color: styleObj.itemColor[0],
+                  },
+                  {
+                    offset: 1,
+                    color: styleObj.itemColor[1],
+                  },
+                ],
+                false,
+              ),
+            },
+          },
+        },
+        {
+          type: 'bar',
+          name: '',
+          stack: 1,
+          barGap: '0',
+          itemStyle: {
+            normal: {
+              color: styleObj.borderColor,
+            },
+          },
+          label: {
+            normal: {
+              show: false,
+            },
+          },
+          data: getBorderHeight(item.data),
+          tooltip: {
+            show: false,
+          },
+        },
+      )
+    } else {
+      seriesData.push(item)
+    }
+  }
+})
 
 @Component({
   selector: 'app-line-bar',
@@ -7,149 +90,78 @@ import * as echarts from 'echarts'
   styleUrls: ['./line-bar.component.less'],
 })
 export class LineBarComponent implements OnInit {
-  chartOptions = {
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'shadow',
-      },
-    },
-    legend: {
-      icon: 'rect',
-      itemWidth: 10,
-      itemHeight: 2,
-      itemGap: 18,
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true,
-    },
-    xAxis: {
-      type: 'category',
-      axisLabel: {
-        color: '#BABBCA',
-        fontSize: 14,
-      },
-      axisLine: {
-        show: false,
-      },
-      splitLine: {
-        show: false,
-      },
-      axisTick: {
-        show: false,
-      },
-      // boundaryGap: false
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-      nameTextStyle: {
-        fontSize: 14,
-        color: '#BABBCA',
-      },
-      axisLabel: {
-        width: 20,
-        fontSize: 14,
-        color: '#BABBCA',
-      },
-      axisLine: {
-        show: false,
-      },
-      axisTick: {
-        show: false,
-      },
-      splitLine: {
-        lineStyle: {
-          // type: 'dashed',
-          color: 'rgba(198, 198, 198, 0.40)',
+  data: any = {}
+  color: any = {}
+  chartOptions: any = {}
+
+  constructor() {
+    this.data = data
+    this.color = color_json
+    this.chartOptions = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
         },
       },
-    },
-    series: [
-      {
-        name: 'Line1',
-        type: 'line',
-        step: 'start',
-        data: [120, 132, 101, 134, 90, 230, 210],
+      legend: {
+        icon: 'rect',
+        itemWidth: 10,
+        itemHeight: 2,
+        itemGap: 18,
+        ...legend,
       },
-      {
-        name: 'Line2',
-        type: 'line',
-        step: 'middle',
-        data: [220, 282, 201, 234, 290, 430, 410],
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
       },
-      {
-        name: 'Line3',
-        type: 'line',
-        step: 'end',
-        data: [450, 432, 401, 454, 590, 530, 510],
-      },
-      {
-        type: 'bar',
-        name: 'Bar1',
-        stack: 1,
-        barGap: '0',
-        borderWidth: 0,
-        itemStyle: {
-          normal: {
-            color: new echarts.graphic.LinearGradient(
-              0,
-              0,
-              0,
-              1,
-              [
-                {
-                  offset: 0,
-                  color: 'rgb(81, 203, 216, 0.5)',
-                },
-                {
-                  offset: 1,
-                  color: 'rgb(81, 203, 216, 0)',
-                },
-              ],
-              false,
-            ),
-          },
+      xAxis: {
+        type: 'category',
+        axisLabel: {
+          color: '#BABBCA',
+          fontSize: 14,
         },
-        label: {
-          normal: {
-            show: false,
-          },
-        },
-        data: [100, 200, 300, 400, 300, 200, 100],
-      },
-      {
-        type: 'bar',
-        name: '',
-        stack: 1,
-        barGap: '0',
-        itemStyle: {
-          normal: {
-            color: 'rgb(121, 238, 255)',
-          },
-        },
-        label: {
-          normal: {
-            show: false,
-          },
-        },
-        data: [10, 10, 10, 10, 10, 10, 10],
-        tooltip: {
+        axisLine: {
           show: false,
         },
+        splitLine: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+        // boundaryGap: false
+        data: xAxisData,
       },
-      {
-        type: 'bar',
-        name: 'Bar2',
-        data: [120, 200, 150, 80, 70, 110, 130],
+      yAxis: {
+        type: 'value',
+        nameTextStyle: {
+          fontSize: 14,
+          color: '#BABBCA',
+        },
+        axisLabel: {
+          width: 20,
+          fontSize: 14,
+          color: '#BABBCA',
+        },
+        axisLine: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+        splitLine: {
+          lineStyle: {
+            // type: 'dashed',
+            color: 'rgba(198, 198, 198, 0.40)',
+          },
+        },
       },
-    ],
+      series: seriesData,
+    }
   }
-
-  constructor() {}
 
   ngOnInit() {}
 }
