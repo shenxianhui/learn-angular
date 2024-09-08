3be617f76d8c9da525f61e888c58087e99881490
shenxh <shenxh0928@qq.com>
2024-09-08 22:34:08 +0800

    feat: slide-list

--- src/app/slide-list/slide-list.component.ts
+++ src/app/slide-list/slide-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { json_data } from './data.json'
+
+const { data_json = {}, color_json = {} } = json_data
+const { data = {} } = data_json
 
 @Component({
   selector: 'app-slide-list',
   templateUrl: './slide-list.component.html',
-  styleUrls: ['./slide-list.component.less']
+  styleUrls: ['./slide-list.component.less'],
 })
 export class SlideListComponent implements OnInit {
+  data: any = {}
+  color: any = {}
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.data = data
+    this.color = color_json
   }
 
+  ngOnInit() {}
 }
