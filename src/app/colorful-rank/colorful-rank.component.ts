8e0881b8c2325eb45d5fa80e44a9aa6331b4b000
shenxh <shenxh0928@qq.com>
2024-09-08 23:03:45 +0800

    feat: colorful-rank

--- src/app/colorful-rank/colorful-rank.component.ts
+++ src/app/colorful-rank/colorful-rank.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { json_data } from './data.json'
+
+const { data_json = {}, color_json = {} } = json_data
+const { data = {} } = data_json
 
 @Component({
   selector: 'app-colorful-rank',
   templateUrl: './colorful-rank.component.html',
-  styleUrls: ['./colorful-rank.component.less']
+  styleUrls: ['./colorful-rank.component.less'],
 })
 export class ColorfulRankComponent implements OnInit {
+  data: any = data
+  color: any = color_json
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
