ef078ea220d9d5cc3fe87375309f74a74b6d5928
shenxh <shenxh0928@qq.com>
2024-10-15 20:00:42 +0800

    feat: details-progress

--- src/app/details-progress/details-progress.component.ts
+++ src/app/details-progress/details-progress.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core'
-import { json_data } from './data.json'
+import schema from './schema.json'
 
-const { data_json = {}, color_json = {} } = json_data
-const { data = {} } = data_json
+interface Format {
+  data: any
+}
+
+const { sampleCode, data: data_json } = schema
+const { format = {} as Format } = data_json || {}
+const { data = {} } = format
+const color_json = sampleCode.replace(/'/g, '"') || '{}'
+const color = JSON.parse(color_json)
 
 @Component({
   selector: 'app-details-progress',
@@ -11,9 +18,31 @@ const { data = {} } = data_json
 })
 export class DetailsProgressComponent implements OnInit {
   data: any = data
-  color: any = color_json
+  color: any = color
 
   constructor() {}
 
   ngOnInit() {}
+
+  getLeft() {
+    const { list } = this.data
+    const num = list.length * 2
+    const left = 100 / num
+
+    return `${left}%`
+  }
+
+  getWidth() {
+    const { list } = this.data
+    const num = list.length * 2
+    const left = (100 / num) * (list.length * 2 - 2)
+
+    return `${left}%`
+  }
+
+  getActWidth() {
+    const { list, activeIndex } = this.data
+
+    return activeIndex * (100 / (list.length - 1)) + '%'
+  }
 }
