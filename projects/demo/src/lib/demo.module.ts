d977901447e64ae368bacc7d2ddd4b7c8098a932
shenxh <xianhui.shen@eslink.com>
2024-11-06 11:24:46 +0800

    feat: 新增 demo 组件

---
+++ projects/demo/src/lib/demo.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { DemoComponent } from './demo.component';
+
+@NgModule({
+  declarations: [DemoComponent],
+  imports: [
+  ],
+  exports: [DemoComponent]
+})
+export class DemoModule { }
